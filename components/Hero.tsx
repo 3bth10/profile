9cad29a597404fd336efdf39b2d40f9ff80a48c0
Mussa Albkery <Mussa Albkery>
2026-04-22 23:42:55 +0300

    fix style .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = ()=>{
     return(
         <div className="p-5 mt-5 w-full text-center " >
             <h4 className="text-start bg-gray-700 rounded-full w-fit px-4  ">
-            <span className="relative animate-pulse mr-3 inline-flex size-3 rounded-full bg-sky-300"></span>
+            <span className="relative animate-pulse mr-3 inline-flex size-3 rounded-full bg-sky-200"></span>
             Available for work </h4>
 
             <div className="grid grid-cols-1 gap-10 lg:grid-cols-2 mt-3 justify-center items-center ">
@@ -18,22 +18,24 @@ const Hero = ()=>{
                     <span className="mt-2 block text-white">Abo Bakrien</span> 
                 </h1> 
                 <h3 className="text-start text-lg text-cyan-500 mt-2">Backend Developer</h3>
-                <p className="mt-3 text-gray-400 text-start w-100 ">Building scalable systems with Python, Django & Next.js Node.js.
+                <p className="mt-3 text-gray-400 text-start  ">Building scalable systems with Python, Django & Next.js Node.js.
                      Passionate about clean architecture and RESTful APIs.</p>
-                     <div className="flex justify-around  lg:w-[50%] mt-10">
+                     <div className="grid grid-cols-2 gap-5 text-base mt-10">
                         <Btn title='View My Work'href='#Projects' />
                         <Btn title='Dwonlad My CV' href={cv} />
                     </div>
                 </div>
                 <div className="">
-                    <div className="rounded-full bg-gray-700 mx-auto w-80 h-80 my-5  ">
+
+                    <div className="rounded-full bg-gray-700 mx-auto w-80 h-80 my-5   ">
                         <img src={Img.src} className="rounded-full border-5 border-cyan-500" alt="MA" />
                     </div>
-                    <div className="flex flex-cols justify-around">
+                    <hr className=" mb-3" />
+                    <div className="grid grid-cols-2 gap-5 ">
                         <Btn title='years +3'/>
                         <Btn title='projects +13'/>
-                        
                     </div>
+
                 </div>
             </div>
         </div>
--- components/header.tsx
+++ components/header.tsx
@@ -2,13 +2,12 @@ const Header = ()=>{
     return (
         <header className="grid grid-cols-2 gap-5  bg-black md:grid-cols-3 text-center w-full shadow-lg shadow-cyan-500/50 p-5 ">
       <h1 className="text-lg text-bold -tracking-2 ">Ar3doi-110</h1>
-      <ul className=" flex gap-5 hidden sm:flex">
+      <ul className=" grid grid-cols-2 text-sm gap-5 lg:text-lg sm:flex">
         <li className="hover:shadow-cyan-500/50 hover:scale-110 transition shadow-lg text-bold "><a href="#home">Home</a></li>
         <li className="hover:shadow-cyan-500/50 hover:scale-110 transition shadow-lg text-bold "><a href="#Projects">Projects</a></li>
         <li className="hover:shadow-cyan-500/50 hover:scale-110 transition shadow-lg text-bold "><a href="#">Contact me</a></li>
         <li className="hover:shadow-cyan-500/50 hover:scale-110 transition  shadow-lg text-bold "><a href='https://github.com/3bth10'> My Github</a> </li>
       </ul>
-      <a href="#" className="bg-cyan-500 w-30 rounded-full py-1 ">Hire me</a>
     </header>
     )
 }
